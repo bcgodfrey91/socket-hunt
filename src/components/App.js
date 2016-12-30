82b290faed40a5f37f3b87d7f4fddde4158d02c2
Ben Godfrey <bcgodfrey91@gmail.com>
2016-12-29 22:52:03 -0600

    Make api call and render results.

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { client_id, client_secret, grant_type } from '../../env.json';
+import { authorization} from '../../env.json';
+import axios from 'axios'
+let endPointURL = "https://api.producthunt.com/v1/";
+
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       posts: [],
-      hasAccessToken: false
     }
   }
 
 componentDidMount() {
+  this.fetchPosts()
+}
+
+fetchPosts() {
+  axios.get('https://api.producthunt.com/v1/posts/all?search[topic]=cats',{
+    headers: {
+      "accept": "application/json",
+      "content-type": "application/json",
+      "authorization": `${authorization}`
+    }
+  })
+  .then((response) => {
+    this.setState({ posts: response.data.posts })
+  })
+  .catch(() => {
+    console.log('nope');
+  })
+}
+
+renderPosts() {
+  return this.state.posts.map((post) => {
+    return (
+      <div className="post" key={post.id}>
+        <h2>{post.name}</h2>
+        <h3>{post.tagline}</h3>
+      </div>
+    )
+  })
 }
 
   render() {
     return (
       <div className="App">
-        <h1>Test</h1>
+        <h1>Socket Hunt</h1>
+        {this.renderPosts()}
       </div>
     );
   }
